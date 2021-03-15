4d2fc1d2d5487b31cfdf7f922c9100aa1cbf048d
kkhaczyn <kkhaczyn@gmail.com>
2021-03-15 12:54:00 +0100

    Add files to repository

---
+++ Lab_1-Haczynski,Jagielski/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("java.specification.version"));
+
+    }
+}
+
